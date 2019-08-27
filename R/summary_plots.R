7a477da45a4cec41a60f6723c7246999900772c0
rathik <rathik@email.chop.edu>
2019-08-27 16:54:46 -0400

    refactor code

--- R/summary_plots.R
+++ R/summary_plots.R
@@ -1,14 +1,16 @@
 # Author: Komal S. Rathi
 # Date: 08/19/2019
 # Function: Create Summary Plots
+# Step 4
 source('~/Projects/DGD_Mendelian_RNASeq/R/pubTheme.R')
 
 # Line plot
 df <- data.frame(Filter = c("F0","F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"), 
-                 Label = c("Combined Calls", "Filter GTEx Recurrent", "Filter Read-throughs", 
-                           "Filter Circular RNAs", "Filter Duplicates", "Filter by Gene Biotype", 
+                 Label = c("Combined Calls", "Filter Circular RNAs", 
+                           "FusionAnnotator", "Filter Read-throughs", 
+                           "Filter Duplicates", "Filter by Gene Biotype", 
                            "Annotation filters", "Expression Filter", "Outlier Analysis"),
-                 Val = c(4926, 4926, 1886, 1850, 1650, 1461, 520, 520, 171))
+                 Val = c(5062, 4846, 4716, 3609, 3596, 3386, 2741, 2739, 364))
 df$Sample <- "GMKF_NBL"
 
 
@@ -20,14 +22,14 @@ p <- ggplot(df, aes(x = Filter, y = Val, group = Sample)) +
   scale_color_discrete(name = "Filters",
                        breaks = c("F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8"),
                        labels = c("F0 = Union Fusion Calls: Arriba + STAR Fusion", 
-                                  "F1 = Filter GTEx Recurrent",
-                                  "F2 = Filter Read-throughs", 
-                                  "F3 = Filter Circular RNAs",
-                                  "F4 = Reduce duplicated fusions with multiple breakpoints",
+                                  "F1 = Filter Circular RNAs",
+                                  "F2 = Filter by FusionAnnotator", 
+                                  "F3 = Filter Read-throughs",
+                                  "F4 = Collapse fusions with multiple breakpoints",
                                   "F5 = Filter by Gene Biotype: Either gene must be Protein-coding",
-                                  "F6 = Create and Merge lists",
+                                  "F6 = Create and Merge lists using various Annotations",
                                   "F7 = Expression Filter: TPM < 1 across all samples",
-                                  "F8 = Outlier Analysis: Sample is outlier for GeneA or GeneB")) +
+                                  "F8 = Tukey's Outlier Analysis: Sample is outlier for GeneA or GeneB")) +
   xlab('Filtering Steps') + ylab('Number of Fusion Calls') +
   ggtitle('Fusion Filteration Pipeline (V1)') + guides(size = F) +
   scale_y_continuous(limits = c(0, 5500), breaks = c(1000, 2000, 3000, 4000, 5000)) 
